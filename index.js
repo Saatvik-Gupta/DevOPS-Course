5f756503cdd3d1a658e77caa7949bc3174fed6c2
Saatvik <shobhita65403@gmail.com>
2026-09-09 21:34:43 +0530

    Code

---
+++ index.js
@@ -0,0 +1,91 @@
+// console.log("Saatvik Gupta");
+
+// var a=10;
+// var b=5;
+// console.log(a+b);
+
+// // alert("Danger!");
+
+// var x;
+// console.log(x);
+
+// function fn(){ // functions are useless until they are called
+//     console.log("Hello");
+// }
+// fn();
+
+// var fn1=function () { console.log("Function in Variable")}
+// fn1();
+
+// console.log(typeof fn1);
+
+// var p=10; // primitive data type
+// var b=3.14;
+// var c="Saatvik";
+// console.log(typeof c);
+
+// // array
+
+// var arr=[1,2,3,4];
+
+// console.log(arr);
+// console.log(typeof arr);
+
+// var arr2=[10,21,-31,100];
+
+// arr2=arr;
+// arr[0]=1000; // non-primitive data type address change of arr & arr2 also
+// console.log(arr2);
+
+// // key value pair
+
+// var student={
+//     name:"Saatvik",
+//     roll_number: 53,
+//     age: 21,
+//     cgpa:[1,2,3,4]
+// };
+
+// console.log(student);
+
+// Javascript is a single threaded synchronous language
+
+// console.log(x); // undefined
+// var x=10;
+// console.log(x) // 10
+// function f(){
+//     console.log("Hello")
+// }
+
+// f();
+
+// console.log("Start");
+// var x=10;
+// console.log(x);
+// console.log(y);
+// var y=20;
+
+// function f(){
+//     var a=1;
+//     var b=2;
+//     console.log(a+b);
+// }
+// console.log(f());
+// console.log("End");
+
+var x=10;
+console.log(x);
+var f1=function(){
+    var a=5;
+    var b=10;
+    return (a*b);
+}
+
+function f2(){
+    var n=50;
+    console.log(f1()+n);
+}
+f2();
+
+
+
