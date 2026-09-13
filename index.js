8782deb4e3486bed45d894bc7b881207656c324c
Saatvik <shobhita65403@gmail.com>
2026-09-13 20:59:42 +0530

    Code

--- index.js
+++ index.js
@@ -1,93 +1,101 @@
-//1. console.log("Saatvik Gupta");
+/*
+// 1. Basics Of Java Script
+ console.log("Saatvik Gupta");
 
-// var a=10;
-// var b=5;
-// console.log(a+b);
+var a=10;
+var b=5;
+console.log(a+b);
 
-//2. alert("Danger!");
+alert("Danger!");
 
-//3. var x;
-// console.log(x);
+// 3.
+ var x;
+console.log(x);
 
-// function fn(){ // functions are useless until they are called
-//     console.log("Hello");
-// }
-// fn();
+function fn(){ // functions are useless until they are called
+    console.log("Hello");
+}
+fn();
 
-//4. var fn1=function () { console.log("Function in Variable")}
-// fn1();
+//4.
+var fn1=function () { console.log("Function in Variable")}
+fn1();
 
-// console.log(typeof fn1);
+console.log(typeof fn1);
 
-//5. var p=10; // primitive data type
-// var b=3.14;
-// var c="Saatvik";
-// console.log(typeof c);
+//5. 
+var p=10; // primitive data type
+var b=3.14;
+var c="Saatvik";
+console.log(typeof c);
 
 //6. array
 
-// var arr=[1,2,3,4];
+var arr=[1,2,3,4];
 
-// console.log(arr);
-// console.log(typeof arr);
+console.log(arr);
+console.log(typeof arr);
 
-// var arr2=[10,21,-31,100];
+var arr2=[10,21,-31,100];
 
-// arr2=arr;
-// arr[0]=1000; // non-primitive data type address change of arr & arr2 also
-// console.log(arr2);
+arr2=arr;
+arr[0]=1000; // non-primitive data type address change of arr & arr2 also
+console.log(arr2);
 
-// // key value pair
+// key value pair
 
-// var student={
-//     name:"Saatvik",
-//     roll_number: 53,
-//     age: 21,
-//     cgpa:[1,2,3,4]
-// };
+var student={
+    name:"Saatvik",
+    roll_number: 53,
+    age: 21,
+    cgpa:[1,2,3,4]
+};
 
-// console.log(student);
+console.log(student); */
 
+/*
 //7. Javascript is a single threaded synchronous language
 
-// console.log(x); // undefined
-// var x=10;
-// console.log(x) // 10
-// function f(){
-//     console.log("Hello")
-// }
+console.log(x); // undefined
+var x=10;
+console.log(x) // 10
+function f(){
+    console.log("Hello")
+}
 
-// f();
+f();
 
-//8. console.log("Start");
-// var x=10;
-// console.log(x);
-// console.log(y);
-// var y=20;
+//8.
+console.log("Start");
+var x=10;
+console.log(x);
+console.log(y);
+var y=20;
 
-// function f(){
-//     var a=1;
-//     var b=2;
-//     console.log(a+b);
-// }
-// console.log(f());
-// console.log("End");
+function f(){
+    var a=1;
+    var b=2;
+    console.log(a+b);
+}
+console.log(f());
+console.log("End");
 
-//9. var x=10;
-// console.log(x);
-// var f1=function(){
-//     var a=5;
-//     var b=10;
-//     return (a*b);
-// }
+//9.
+var x=10;
+console.log(x);
+var f1=function(){
+    var a=5;
+    var b=10;
+    return (a*b);
+}
 
-// function f2(){
-//     var n=50;
-//     console.log(f1()+n);
-// }
-// f2();
+function f2(){
+    var n=50;
+    console.log(f1()+n);
+}
+f2();
 
-// console.log(typeof f1); // function
+console.log(typeof f1); // function */
 
 //10. lexigonal environment and closure concept      
 
