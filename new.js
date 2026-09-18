ad9117a438bdaa1f4aecea54029240b6cf97a4ea
Saatvik <shobhita65403@gmail.com>
2026-09-18 16:49:39 +0530

    Code

--- new.js
+++ new.js
@@ -1,1 +1,11 @@
-let x=10;
+// const body=document.body;
+// const div1=body.children[0]; // we got div as a collection or array
+// const h1=div1.children[0];
+// h1.innerHTML="Value Changed by using JS";
+
+// const h1=document.getElementById("head1");
+// h1.innerText="Again Changed";
+
+// element creation
+const footerDiv=document.createElement("div");
+footerDiv.innerText="This is a Footer Div";
