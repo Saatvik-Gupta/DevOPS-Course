cbfd0587e3e3dd1e56a76b2453e116d457c533ba
Saatvik <shobhita65403@gmail.com>
2026-09-25 17:01:11 +0530

    Mini Project

--- example.js
+++
@@ -1,8 +0,0 @@
-const footerDiv=document.createElement("div");
-footerDiv.append("Saatvik Gupta");
-
-const paragraph=document.createElement("p");
-
-paragraph.append("Undergraduate AIML Student,3rd Year");
-paragraph.append("Currently Studing DEVOPS");
-
